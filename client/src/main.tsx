5f85f68b4762a877a27eef92d3e0598b2001ba8c
dnhyxc <2640559435@qq.com>
2025-12-16 09:53:37 +0800

    chore: test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from '@/router';
 import './index.css';
 
+const a = 1;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<App />
