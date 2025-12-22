07845428958e5e55767eae1d0cb358c80c637edb
dnhyxc <2640559435@qq.com>
2025-12-22 09:35:25 +0800

    feat: 使用 typeorm 链接数据库同时利用 typeorm 创建表结构

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Global, Logger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 import * as Joi from 'joi';
 import { LogsModule } from './logs/logs.module';
 import { PromptModule } from './prompt/prompt.module';
+import { ConfigEnum } from './enum/config.enum';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 
@@ -23,12 +27,47 @@ const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 			// TODO: Joi 配置未生效，后续完善
 			validationSchema: Joi.object({
 				// 使用 Joi 验证环境变量中的DB_PORT
-				DB_PORT: Joi.number().default(3306),
 				NODE_ENV: Joi.string()
 					.valid('development', 'production')
 					.default('development'),
+				DB_PORT: Joi.number().default(3090),
+				DB_HOST: Joi.string().ip(),
+				DB_TYPE: Joi.string().valid('mysql', 'postgres'),
+				DB_USERNAME: Joi.string().required(),
+				DB_PASSWORD: Joi.string().required(),
+				DB_DATABASE: Joi.string().required(),
+				DB_SYNC: Joi.boolean().default(false),
 			}),
 		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) =>
+				({
+					type: configService.get(ConfigEnum.DB_TYPE),
+					host: configService.get(ConfigEnum.DB_HOST),
+					port: configService.get(ConfigEnum.DB_PORT),
+					username: configService.get(ConfigEnum.DB_USERNAME),
+					password: configService.get(ConfigEnum.DB_PASSWORD),
+					database: configService.get(ConfigEnum.DB_DATABASE),
+					entities: [User],
+					synchronize: configService.get(ConfigEnum.DB_SYNC),
+					logging: ['error'],
+				}) as TypeOrmModuleOptions,
+		}),
+		UserModule,
+		// TypeOrmModule.forRoot({
+		// 	type: 'mysql',
+		// 	host: 'localhost',
+		// 	port: 3090, // 这里必须在 docker-compose.yml 中的 db - ports 中暴露出来才能正常链接数据库
+		// 	username: 'root',
+		// 	password: 'example',
+		// 	database: 'testdb',
+		// 	entities: [],
+		// 	// 同步本地的 schema 与数据库 -> 初始化的时候使用
+		// 	synchronize: true,
+		// 	logging: ['error'],
+		// }),
 	],
 	controllers: [],
 	providers: [Logger],
