23de1a4ec31d790cf7745ad089339fe2b1b374a7
dnhyxc <2640559435@qq.com>
2025-12-22 14:49:56 +0800

    feat: 提交 logs

--- server/src/logs/logs.entity.ts
+++ server/src/logs/logs.entity.ts
@@ -1,5 +1,11 @@
 import { User } from 'src/user/user.entity';
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+	Column,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Logs {
@@ -18,9 +24,12 @@ export class Logs {
 	@Column()
 	result: number;
 
-  // 表示多个用户对应一个用户
-  @ManyToOne(() => User, (user) => user.logs)
-  // 表明在 user 表中建立关联关系，即默认会在 logs 表中生成一个 userId 外键字段
-  @JoinColumn()
-  user: User;
+	// 表示多个用户对应一个用户
+	@ManyToOne(
+		() => User,
+		(user) => user.logs,
+	)
+	// 表明在 user 表中建立关联关系，即默认会在 logs 表中生成一个 userId 外键字段
+	@JoinColumn()
+	user: User;
 }
--- server/src/logs/logs.module.ts
+++ server/src/logs/logs.module.ts
@@ -1,67 +1,71 @@
 import { Module } from '@nestjs/common';
-import { WinstonModule, utilities } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
+import { utilities, WinstonModule } from 'nest-winston';
+import { LogEnum } from 'src/enum/config.enum';
+import * as winston from 'winston';
 // import { ConfigService } from '@nestjs/config';
 // import { type ConfigService } from '@nestjs/config';
 import { Console } from 'winston/lib/winston/transports';
-import * as winston from 'winston';
-import DailyRotateFile from 'winston-daily-rotate-file'
-import { ConfigService } from '@nestjs/config';
-import { LogEnum } from 'src/enum/config.enum';
+import DailyRotateFile from 'winston-daily-rotate-file';
 // import { LogEnum } from 'src/enum/config.enum';
 // import 'winston-daily-rotate-file';
 
 @Module({
-  imports: [
-    WinstonModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>{
-        const consoleTransports = new Console({
-        	level: 'info',
-        	format: winston.format.combine(
-        		winston.format.timestamp(),
-        		utilities.format.nestLike(),
-        	),
-        })
+	imports: [
+		WinstonModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => {
+				const consoleTransports = new Console({
+					level: 'info',
+					format: winston.format.combine(
+						winston.format.timestamp(),
+						utilities.format.nestLike(),
+					),
+				});
 
-        const dailyTransports = new DailyRotateFile({
-        	level: 'warn',
-        	// TODO: 生产环境 dirname 指定一个专门存放日志的绝对路径，开发环境这里直接指定项目本地路径
-        	dirname: 'logs',
-        	filename: 'application-%DATE%.log',
-        	datePattern: 'YYYY-MM-DD-HH',
-        	zippedArchive: true,
-        	maxSize: '20m',
-        	maxFiles: '14d',
-        	format: winston.format.combine(
-        		winston.format.timestamp(),
-        		winston.format.simple(),
-        		// utilities.format.nestLike(),
-        	),
-        });
+				const dailyTransports = new DailyRotateFile({
+					level: 'warn',
+					// TODO: 生产环境 dirname 指定一个专门存放日志的绝对路径，开发环境这里直接指定项目本地路径
+					dirname: 'logs',
+					filename: 'application-%DATE%.log',
+					datePattern: 'YYYY-MM-DD-HH',
+					zippedArchive: true,
+					maxSize: '20m',
+					maxFiles: '14d',
+					format: winston.format.combine(
+						winston.format.timestamp(),
+						winston.format.simple(),
+						// utilities.format.nestLike(),
+					),
+				});
 
-        const dailyInfoTransports = new DailyRotateFile({
-        	level: configService.get(LogEnum.LOG_LEVEL),
-        	// TODO: 生产环境 dirname 指定一个专门存放日志的绝对路径，开发环境这里直接指定项目本地路径
-        	dirname: 'logs',
-        	filename: 'info-%DATE%.log',
-        	datePattern: 'YYYY-MM-DD-HH',
-        	zippedArchive: true,
-        	maxSize: '20m',
-        	maxFiles: '14d',
-        	format: winston.format.combine(
-        		winston.format.timestamp(),
-        		winston.format.simple(),
-        		// utilities.format.nestLike(),
-        	),
-        });
+				const dailyInfoTransports = new DailyRotateFile({
+					level: configService.get(LogEnum.LOG_LEVEL),
+					// TODO: 生产环境 dirname 指定一个专门存放日志的绝对路径，开发环境这里直接指定项目本地路径
+					dirname: 'logs',
+					filename: 'info-%DATE%.log',
+					datePattern: 'YYYY-MM-DD-HH',
+					zippedArchive: true,
+					maxSize: '20m',
+					maxFiles: '14d',
+					format: winston.format.combine(
+						winston.format.timestamp(),
+						winston.format.simple(),
+						// utilities.format.nestLike(),
+					),
+				});
 
-        return {
-          transports: [consoleTransports, ...(configService.get(LogEnum.LOG_ON) ? [dailyTransports, dailyInfoTransports] : [])]
-          // transports: [consoleTransports, dailyTransports, dailyInfoTransports]
-        }
-      },
-    }),
-  ],
+				return {
+					transports: [
+						consoleTransports,
+						...(configService.get(LogEnum.LOG_ON)
+							? [dailyTransports, dailyInfoTransports]
+							: []),
+					],
+					// transports: [consoleTransports, dailyTransports, dailyInfoTransports]
+				};
+			},
+		}),
+	],
 })
-
 export class LogsModule {}
