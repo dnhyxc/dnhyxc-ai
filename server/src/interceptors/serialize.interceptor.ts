c144d2e5784029cd31402611222b86d5308a8aad
dnhyxc <2640559435@qq.com>
2025-12-24 13:54:40 +0800

    feat: 自定义拦截器

--- server/src/interceptors/serialize.interceptor.ts
+++ server/src/interceptors/serialize.interceptor.ts
@@ -4,17 +4,25 @@ import {
 	Injectable,
 	NestInterceptor,
 } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class SerializeInterceptor implements NestInterceptor {
+	constructor(private readonly dto: any) {}
 	intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
 		// const req = context.switchToHttp().getRequest();
 		// 这会在拦截器之前执行
 		return next.handle().pipe(
 			map((data) => {
 				// 这里会在拦截器之后执行
-				return data;
+				return plainToInstance(this.dto, data, {
+					/**
+					 * 设置完 excludeExtraneousValues: true 后，所有经过 interceptor 的接口都需要设置 Expose 或 Exclude
+					 * Expose 表示当前字段需要暴露，Exclude 表示当前字段不需要暴露
+					 */
+					excludeExtraneousValues: true,
+				});
 			}),
 		);
 	}
--- server/src/logs/logs.controller.ts
+++ server/src/logs/logs.controller.ts
@@ -1,6 +1,28 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Expose } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Serialize } from '../decorators/serialize.decorator';
 import { AdminGuard } from '../guards/admin.guard';
 import { JwtGuard } from '../guards/jwt.guard';
+
+class LogsDto {
+	@IsString()
+	@IsNotEmpty()
+	msg: string;
+
+	@IsString()
+	id: string;
+
+	@IsString()
+	name: string;
+}
+
+class PublicLogsDto {
+	@Expose()
+	msg: string;
+	@Expose()
+	name: string;
+}
 
 @Controller('logs')
 @UseGuards(JwtGuard, AdminGuard)
@@ -8,5 +30,15 @@ export class LogsController {
 	@Get('/getLogs')
 	getLogs() {
 		return 'get logs';
+	}
+
+	@Post('/addLogs')
+	// 添加 SerializeInterceptor 后置拦截器对响应字段进行序列化
+	// @UseInterceptors(new SerializeInterceptor(PublicLogsDto))
+	// 使用自定义的 Serialize 系列化装饰器对响应字段进行序列化
+	@Serialize(PublicLogsDto)
+	addLogs(@Body() dto: LogsDto) {
+		console.log(dto, '参数dto');
+		return dto;
 	}
 }
