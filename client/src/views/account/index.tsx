c19e302507e2d188766546e5e3acfb18db4a7e91
dnhyxc <2640559435@qq.com>
2026-01-14 10:26:13 +0800

    feat: 加入loading

--- client/src/views/account/index.tsx
+++ client/src/views/account/index.tsx
@@ -1,4 +1,3 @@
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@ui/button';
 import { Input } from '@ui/input';
 import { Label } from '@ui/label';
@@ -7,19 +6,10 @@ import { ScrollArea } from '@ui/scroll-area';
 import { Toast } from '@ui/sonner';
 import { SquarePen } from 'lucide-react';
 import { useEffect, useMemo, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
 import Model from '@/components/design/Model';
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from '@/components/ui/form';
-import { sendEmail, updateEmail, updateUser } from '@/service';
+import { updateUser } from '@/service';
 import { getStorage, setStorage } from '@/utils';
+import ResetEmailForm from './reset-email-form';
 
 const GenderEnum: Record<string, string> = {
 	1: '男',
@@ -37,10 +27,6 @@ const Account = () => {
 		gender: '3',
 		address: '',
 		avatar: '',
-	});
-	const [verifyCodeInfo, setVerifyCodeInfo] = useState({
-		oldVerifyCodeKey: '',
-		newVerifyCodeKey: '',
 	});
 
 	const userInfo = useMemo(
@@ -208,101 +194,15 @@ const Account = () => {
 		setEditKey('');
 	};
 
+	const handleAccountInfo = (email: string) => {
+		setAccountInfo({
+			...accountInfo,
+			email,
+		});
+	};
+
 	const onOpenChange = () => {
 		setOpen(false);
-		form.reset();
-		setVerifyCodeInfo({
-			oldVerifyCodeKey: '',
-			newVerifyCodeKey: '',
-		});
-	};
-
-	const onSendEmail = async (
-		e: React.MouseEvent<HTMLButtonElement>,
-		key: string,
-	) => {
-		e.preventDefault();
-		const email = key === 'old' ? userInfo.email : form.watch('email');
-		const res = await sendEmail(email, {
-			key: key === 'old' ? 'OLD_EMAIL' : 'NEW_EMAIL',
-			timeout: 300 * 1000,
-		});
-		if (res.code === 200) {
-			if (key === 'old') {
-				setVerifyCodeInfo({
-					...verifyCodeInfo,
-					oldVerifyCodeKey: res.data.key,
-				});
-			} else {
-				setVerifyCodeInfo({
-					...verifyCodeInfo,
-					newVerifyCodeKey: res.data.key,
-				});
-			}
-		}
-		Toast({
-			title: res.success ? '获取验证码成功' : '获取验证码失败',
-			type: res.success ? 'success' : 'error',
-		});
-	};
-
-	const formSchema = z.object({
-		email: z
-			.string()
-			.trim()
-			.regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, { message: '请输入合法的邮箱地址' }),
-		oldVerifyCode: z
-			.string()
-			.trim()
-			.min(6, { message: '验证码至少输入6个字符' }),
-		newVerifyCode: z
-			.string()
-			.trim()
-			.min(6, { message: '验证码至少输入6个字符' }),
-	});
-
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			email: '',
-			oldVerifyCode: '',
-			newVerifyCode: '',
-		},
-	});
-
-	const onSubmitEmail = async (values: z.infer<typeof formSchema>) => {
-		if (!verifyCodeInfo.oldVerifyCodeKey || !verifyCodeInfo.newVerifyCodeKey) {
-			return Toast({
-				type: 'info',
-				title: '验证码 Key 不能为空',
-			});
-		}
-		const res = await updateEmail({
-			id: userInfo.id,
-			email: values.email,
-			oldVerifyCode: values.oldVerifyCode,
-			newVerifyCode: values.newVerifyCode,
-			oldVerifyCodeKey: verifyCodeInfo.oldVerifyCodeKey,
-			newVerifyCodeKey: verifyCodeInfo.newVerifyCodeKey,
-		});
-		if (res.code === 200) {
-			setStorage(
-				'userInfo',
-				JSON.stringify({
-					...userInfo,
-					email: values.email,
-				}),
-			);
-			setAccountInfo({
-				...accountInfo,
-				email: values.email,
-			});
-			onOpenChange();
-		}
-		Toast({
-			title: res.success ? '修改成功' : '修改失败',
-			type: res.success ? 'success' : 'error',
-		});
 	};
 
 	return (
@@ -372,87 +272,11 @@ const Account = () => {
 				footer={null}
 				onOpenChange={onOpenChange}
 			>
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmitEmail)}
-						className="space-y-6"
-					>
-						<div className="flex flex-col">
-							<div className="my-2 font-medium">原邮箱</div>
-							<Input value={userInfo?.email} disabled />
-						</div>
-						<FormField
-							control={form.control}
-							name="oldVerifyCode"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel className="text-md">原邮箱验证码</FormLabel>
-									<FormControl>
-										<div className="flex items-center">
-											<Input placeholder="请输入原邮箱验证码" {...field} />
-											<Button
-												type="button"
-												className="ml-2 cursor-pointer"
-												onClick={(e) => onSendEmail(e, 'old')}
-											>
-												获取验证码
-											</Button>
-										</div>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="email"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel className="text-md">新邮箱</FormLabel>
-									<FormControl>
-										<Input type="email" placeholder="请输入新邮箱" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<FormField
-							control={form.control}
-							name="newVerifyCode"
-							render={({ field }) => (
-								<FormItem>
-									<FormLabel className="text-md">新邮箱验证码</FormLabel>
-									<FormControl>
-										<div className="flex items-center">
-											<Input placeholder="请输入新邮箱验证码" {...field} />
-											<Button
-												type="button"
-												className="ml-2 cursor-pointer"
-												onClick={(e) => onSendEmail(e, 'new')}
-											>
-												获取验证码
-											</Button>
-										</div>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-						<div className="flex items-center justify-end mt-8">
-							<Button type="submit" className="cursor-pointer mr-2 w-20">
-								确定
-							</Button>
-							<Button
-								variant="outline"
-								type="reset"
-								className="cursor-pointer w-20"
-								onClick={onOpenChange}
-							>
-								取消
-							</Button>
-						</div>
-					</form>
-				</Form>
+				<ResetEmailForm
+					userInfo={userInfo}
+					onOpenChange={onOpenChange}
+					handleAccountInfo={handleAccountInfo}
+				/>
 			</Model>
 		</div>
 	);
---
+++ client/src/views/account/reset-email-form.tsx
@@ -0,0 +1,244 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@ui/button';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@ui/form';
+import { Input } from '@ui/input';
+import { Toast } from '@ui/sonner';
+import { Spinner } from '@ui/spinner';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { sendEmail, updateEmail } from '@/service';
+import { setStorage } from '@/utils';
+
+interface IProps {
+	userInfo: any;
+	onOpenChange: () => void;
+	handleAccountInfo: (email: string) => void;
+}
+
+const ResetEmailForm: React.FC<IProps> = ({
+	userInfo,
+	onOpenChange,
+	handleAccountInfo,
+}) => {
+	const [verifyCodeInfo, setVerifyCodeInfo] = useState({
+		oldVerifyCodeKey: '',
+		newVerifyCodeKey: '',
+	});
+	const [sendOldLoading, setSendOldLoading] = useState(false);
+	const [sendNewLoading, setSendNewLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const formSchema = z.object({
+		email: z
+			.string()
+			.trim()
+			.regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, { message: '请输入合法的邮箱地址' }),
+		oldVerifyCode: z
+			.string()
+			.trim()
+			.min(6, { message: '验证码至少输入6个字符' }),
+		newVerifyCode: z
+			.string()
+			.trim()
+			.min(6, { message: '验证码至少输入6个字符' }),
+	});
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: '',
+			oldVerifyCode: '',
+			newVerifyCode: '',
+		},
+	});
+
+	const onSendEmail = async (
+		e: React.MouseEvent<HTMLButtonElement>,
+		key: string,
+	) => {
+		e.preventDefault();
+		if (key === 'old') {
+			setSendOldLoading(true);
+		} else {
+			setSendNewLoading(true);
+		}
+		const email = key === 'old' ? userInfo.email : form.watch('email');
+		const res = await sendEmail(email, {
+			key: key === 'old' ? 'OLD_EMAIL' : 'NEW_EMAIL',
+			timeout: 300 * 1000,
+		});
+		if (key === 'old') {
+			setSendOldLoading(false);
+		} else {
+			setSendNewLoading(false);
+		}
+		if (res.code === 200) {
+			if (key === 'old') {
+				setVerifyCodeInfo({
+					...verifyCodeInfo,
+					oldVerifyCodeKey: res.data.key,
+				});
+			} else {
+				setVerifyCodeInfo({
+					...verifyCodeInfo,
+					newVerifyCodeKey: res.data.key,
+				});
+			}
+		}
+		Toast({
+			title: res.success ? '获取验证码成功' : '获取验证码失败',
+			type: res.success ? 'success' : 'error',
+		});
+	};
+
+	const onSubmitEmail = async (values: z.infer<typeof formSchema>) => {
+		if (!verifyCodeInfo.oldVerifyCodeKey || !verifyCodeInfo.newVerifyCodeKey) {
+			return Toast({
+				type: 'info',
+				title: '验证码 Key 不能为空',
+			});
+		}
+		setLoading(true);
+		const res = await updateEmail({
+			id: userInfo.id,
+			email: values.email,
+			oldVerifyCode: values.oldVerifyCode,
+			newVerifyCode: values.newVerifyCode,
+			oldVerifyCodeKey: verifyCodeInfo.oldVerifyCodeKey,
+			newVerifyCodeKey: verifyCodeInfo.newVerifyCodeKey,
+		});
+		setLoading(false);
+		if (res.code === 200) {
+			setStorage(
+				'userInfo',
+				JSON.stringify({
+					...userInfo,
+					email: values.email,
+				}),
+			);
+			handleAccountInfo?.(values.email);
+			onOpenChange?.();
+			form.reset();
+			setVerifyCodeInfo({
+				oldVerifyCodeKey: '',
+				newVerifyCodeKey: '',
+			});
+		}
+		Toast({
+			title: res.success ? '修改成功' : '修改失败',
+			type: res.success ? 'success' : 'error',
+		});
+	};
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmitEmail)} className="space-y-6">
+				<div className="flex flex-col">
+					<div className="my-2 font-medium">原邮箱</div>
+					<Input value={userInfo?.email} disabled />
+				</div>
+				<FormField
+					control={form.control}
+					name="oldVerifyCode"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel className="text-md">原邮箱验证码</FormLabel>
+							<FormControl>
+								<div className="flex items-center">
+									<Input
+										maxLength={6}
+										placeholder="请输入原邮箱验证码"
+										{...field}
+									/>
+									<Button
+										type="button"
+										className="ml-2 cursor-pointer"
+										disabled={sendOldLoading}
+										onClick={(e) => onSendEmail(e, 'old')}
+									>
+										{sendOldLoading ? <Spinner /> : null}
+										获取验证码
+									</Button>
+								</div>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{verifyCodeInfo.oldVerifyCodeKey ? (
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className="text-md">新邮箱</FormLabel>
+								<FormControl>
+									<Input type="email" placeholder="请输入新邮箱" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				) : null}
+				{verifyCodeInfo.oldVerifyCodeKey ? (
+					<FormField
+						control={form.control}
+						name="newVerifyCode"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className="text-md">新邮箱验证码</FormLabel>
+								<FormControl>
+									<div className="flex items-center">
+										<Input
+											maxLength={6}
+											placeholder="请输入新邮箱验证码"
+											{...field}
+										/>
+										<Button
+											type="button"
+											className="ml-2 cursor-pointer flex items-center"
+											disabled={sendNewLoading || !form.watch('email')}
+											onClick={(e) => onSendEmail(e, 'new')}
+										>
+											{sendNewLoading ? <Spinner /> : null}
+											获取验证码
+										</Button>
+									</div>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				) : null}
+				<div className="flex items-center justify-end mt-8">
+					<Button
+						type="submit"
+						disabled={loading}
+						className="cursor-pointer mr-2 w-20"
+					>
+						{loading ? <Spinner /> : null}
+						确定
+					</Button>
+					<Button
+						variant="outline"
+						type="reset"
+						className="cursor-pointer w-20"
+						onClick={onOpenChange}
+					>
+						取消
+					</Button>
+				</div>
+			</form>
+		</Form>
+	);
+};
+
+export default ResetEmailForm;
