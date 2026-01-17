a560075b5a6519e7582e3c458eb9e4f4d8557a59
dnhyxc <2640559435@qq.com>
2026-01-17 12:07:10 +0800

    feat: 更改配置文件

---
+++ apps/backend/src/services/logs/logs.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+// import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class LogsService {
+	// constructor(private readonly userService: UserService) {
+	// 	console.log(this.userService);
+	// }
+}
