8a33443695f74a617af0bf03cb8a8528dd6374b1
dnhyxc <2640559435@qq.com>
2026-01-21 14:13:55 +0800

    feat: 富文本编辑器

--- apps/frontend/src/components/design/Monaco/index.tsx
+++ apps/frontend/src/components/design/Monaco/index.tsx
@@ -1,6 +1,7 @@
 import Editor, { type OnMount } from '@monaco-editor/react';
 import { useRef } from 'react';
 import { cn } from '@/lib/utils';
+import { options } from './options';
 
 interface MarkdownEditorProps {
 	value?: string;
@@ -10,6 +11,7 @@ interface MarkdownEditorProps {
 	height?: string;
 	readOnly?: boolean;
 	theme?: 'vs' | 'vs-dark' | 'hc-black';
+	language?: string;
 }
 
 const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
@@ -20,6 +22,7 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
 	height = '400px',
 	readOnly = false,
 	theme = 'vs',
+	language = 'markdown',
 }) => {
 	const editorRef = useRef<Parameters<OnMount>[0] | null>(null);
 
@@ -54,47 +57,12 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
 			</div>
 			<Editor
 				height={height}
-				language="markdown"
+				language={language}
 				value={value}
 				onChange={(val) => onChange?.(val || '')}
 				theme={theme}
 				onMount={handleEditorMount}
-				options={{
-					readOnly, // 是否只读
-					placeholder, // 占位文本
-					minimap: { enabled: false }, // 关闭右侧缩略图
-					fontSize: 14, // 字号
-					lineHeight: 22, // 行高
-					fontFamily:
-						'Fira Code, "SF Mono", "Monaco", "Menlo", "Consolas", monospace', // 字体
-					fontLigatures: true, // 启用连字
-					lineNumbers: 'on', // 显示行号
-					wordWrap: 'on', // 自动换行
-					colorDecorators: true, // 呈现内联色彩装饰器和颜色选择器
-					scrollBeyondLastLine: false, // 禁止滚动到最后一行之后
-					automaticLayout: true, // 自动调整布局
-					padding: { top: 10, bottom: 10 }, // 上下内边距
-					quickSuggestions: false, // 禁用快速建议
-					suggestOnTriggerCharacters: false, // 禁用触发字符建议
-					tabSize: 2, // 缩进大小
-					folding: true, // 是否启用代码折叠
-					foldingHighlight: true, // 折叠时是否高亮
-					foldingStrategy: 'indentation', // 代码折叠策略，使用缩进方式
-					wordBasedSuggestions: 'allDocuments', // 关闭基于单词的建议
-					parameterHints: { enabled: false }, // 关闭参数提示
-					snippetSuggestions: 'inline', // 代码片段建议显示方式：inline（内联）
-					cursorBlinking: 'smooth', // 光标平滑闪烁
-					cursorSmoothCaretAnimation: 'off', // 启用光标平滑动画
-					renderLineHighlight: 'none', // 高亮当前行
-					hideCursorInOverviewRuler: true, // 在概览标尺中隐藏光标
-					overviewRulerBorder: false, // 关闭概览标尺边框
-					scrollbar: {
-						vertical: 'visible', // 垂直滚动条可见
-						horizontal: 'visible', // 水平滚动条可见
-						verticalScrollbarSize: 8, // 垂直滚动条宽度
-						horizontalScrollbarSize: 8, // 水平滚动条高度
-					},
-				}}
+				options={{ ...options, readOnly, placeholder }}
 				loading={
 					<div className="flex items-center justify-center w-full h-full text-textcolor bg-theme-background/20">
 						Loading...
--- apps/frontend/src/views/editor/index.tsx
+++ apps/frontend/src/views/editor/index.tsx
@@ -5,7 +5,10 @@ const Editor = () => {
 	return (
 		<div className="w-full h-full flex flex-col justify-center items-center m-0">
 			<ScrollArea className="w-full h-full overflow-y-auto p-2.5 pt-0 rounded-none">
-				<MarkdownEditor />
+				<MarkdownEditor
+					className="w-full h-full"
+					height="calc(100vh - 161px)"
+				/>
 			</ScrollArea>
 		</div>
 	);
