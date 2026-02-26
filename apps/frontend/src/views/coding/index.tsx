2687b700071e8201cf5cfce6261c15336d3eceb0
dnhyxc <2640559435@qq.com>
2026-02-26 15:41:01 +0800

    feat: 完善coding页面布局

--- apps/frontend/src/views/coding/index.tsx
+++ apps/frontend/src/views/coding/index.tsx
@@ -5,13 +5,14 @@ import {
 	SandpackProvider,
 } from '@codesandbox/sandpack-react';
 import { ScrollArea } from '@radix-ui/react-scroll-area';
-import { useState } from 'react';
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup,
+} from '@/components/ui/resizable';
 import Toolbar from './toolbar';
 
 const CodeRunner = () => {
-	const [isPreviewVisible, setIsPreviewVisible] = useState(true);
-	const [isConsoleVisible, setIsConsoleVisible] = useState(true);
-
 	return (
 		<div className="flex flex-col h-full w-full rounded-b-md">
 			<ScrollArea className="flex-1 rounded-md overflow-hidden w-full backdrop-blur-sm">
@@ -28,33 +29,44 @@ const CodeRunner = () => {
 						>
 							<div className="flex flex-col h-full">
 								{/* 顶部工具栏 */}
-								<Toolbar
-									isPreviewVisible={isPreviewVisible}
-									onTogglePreview={() => setIsPreviewVisible(!isPreviewVisible)}
-									isConsoleVisible={isConsoleVisible}
-									onToggleConsole={() => setIsConsoleVisible(!isConsoleVisible)}
-								/>
-
-								<div className="flex flex-1 overflow-hidden rounded-b-md">
-									{/* 左侧编辑器 */}
-									<SandpackCodeEditor
-										className={`${isPreviewVisible || isConsoleVisible ? 'w-[50%]!' : 'w-full!'}`}
-									/>
-
-									{/* 右侧区域 */}
-									<div className="flex-1 flex flex-col border-l border-theme/5">
-										{/* UI 预览 */}
-										<SandpackPreview
-											showRestartButton
-											showOpenInCodeSandbox={false}
-											className={`flex-1! ${isConsoleVisible ? 'h-[50%]!' : 'h-full!'} ${isPreviewVisible ? 'flex!' : 'hidden!'}`}
-										/>
-										{/* 控制台 */}
-										<SandpackConsole
-											className={`flex-1! ${isPreviewVisible ? 'h-[50%]!' : 'h-full!'} ${isConsoleVisible ? 'flex!' : 'hidden!'}`}
-											resetOnPreviewRestart
-										/>
-									</div>
+								<Toolbar />
+								<div className="flex flex-1 rounded-b-md">
+									<ResizablePanelGroup
+										orientation="horizontal"
+										className="h-full w-full rounded-b-md"
+									>
+										{/* 左侧编辑器 */}
+										<ResizablePanel defaultSize="50%" className="w-full h-full">
+											<SandpackCodeEditor className="h-full" />
+										</ResizablePanel>
+										<ResizableHandle withHandle />
+										{/* 右侧区域 */}
+										<ResizablePanel defaultSize="50%" className="w-full h-full">
+											<div className="flex-1 flex flex-col h-full">
+												{/* UI 预览 */}
+												<ResizablePanelGroup
+													orientation="vertical"
+													className="flex-1 flex flex-col h-full"
+												>
+													<ResizablePanel defaultSize="50%">
+														<SandpackPreview
+															showRestartButton
+															showOpenInCodeSandbox={false}
+															className="flex-1! h-full! w-full!"
+														/>
+													</ResizablePanel>
+													<ResizableHandle withHandle />
+													{/* 控制台 */}
+													<ResizablePanel defaultSize="50%">
+														<SandpackConsole
+															resetOnPreviewRestart
+															className="w-full! flex-1! h-full!"
+														/>
+													</ResizablePanel>
+												</ResizablePanelGroup>
+											</div>
+										</ResizablePanel>
+									</ResizablePanelGroup>
 								</div>
 							</div>
 						</SandpackProvider>
