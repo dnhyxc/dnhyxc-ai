599fdf386cf2d2b725516ce892d7db823d29ab98
dnhyxc <2640559435@qq.com>
2026-03-17 22:04:41 +0800

    feat: 解决刷新后，store 中messages被清除导致会话内容不展示的问题

--- apps/frontend/src/views/chat/session/index.tsx
+++ apps/frontend/src/views/chat/session/index.tsx
@@ -1,6 +1,29 @@
 import ChatBot from '@design/ChatBot';
+import { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { getSession } from '@/service';
+import useStore from '@/store';
 
-const Chat = () => {
+const Session = () => {
+	const params = useParams();
+
+	const { chatStore } = useStore();
+
+	useEffect(() => {
+		// 只有在刷新后，store 中的 messages 为空的时候才调用接口获取 messages
+		if (params.id && !chatStore.messages.length) {
+			getSessionInfo(params.id);
+		}
+	}, [params.id, chatStore.messages]);
+
+	const getSessionInfo = async (id: string) => {
+		const res = await getSession(id);
+		if (res.success && res.data.messages.length) {
+			chatStore.setAllMessages(res.data.messages, id, false);
+			chatStore.setActiveSessionId(id);
+		}
+	};
+
 	return (
 		<div className="flex-1 w-full overflow-hidden">
 			<ChatBot />
@@ -8,4 +31,4 @@ const Chat = () => {
 	);
 };
 
-export default Chat;
+export default Session;
