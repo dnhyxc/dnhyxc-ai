831f4fa88819cc912182b7575f64ce073881b45a
dnhyxc <2640559435@qq.com>
2026-03-31 12:32:32 +0800

    fix: 解决从别的路由进入到chat时沿用之前的sessionId

--- apps/frontend/src/views/chat/new/index.tsx
+++ apps/frontend/src/views/chat/new/index.tsx
@@ -1,7 +1,16 @@
 import ChatNewSession from '@design/ChatNewSession';
 import { observer } from 'mobx-react';
+import { useEffect } from 'react';
+import { useChatCore } from '@/hooks/useChatCore';
 
 const NewChat = observer(() => {
+	const { clearChat } = useChatCore();
+
+	// 从其他路由进入到新会话页时，清空当前会话，比如从 home 页面进入时，如果不清空，会继续使用上次会话的 sessionId，将内容添加到之前的会话中
+	useEffect(() => {
+		clearChat();
+	}, []);
+
 	return (
 		<div className="flex-1 w-full overflow-hidden">
 			<ChatNewSession />
