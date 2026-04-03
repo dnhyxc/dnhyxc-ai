2cad31423d5fe6d6284f4120acc7054978be9949
dnhyxc <2640559435@qq.com>
2026-04-03 12:07:09 +0800

    chore: Commitlint 修改

--- commitlint.config.js
+++ commitlint.config.js
@@ -25,7 +25,8 @@ export default {
 		],
 		'type-case': [2, 'always', 'lower-case'],
 		'type-empty': [2, 'never'],
-		'subject-case': [2, 'always', 'lower-case'],
+		// 关闭全小写限制，允许 feat: 后使用英文大写（如专有名词、API 名）
+		'subject-case': [0],
 		'subject-empty': [2, 'never'],
 		'header-max-length': [0, 'always', 72],
 	},
