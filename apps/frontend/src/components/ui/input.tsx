e23924e4fcefa927e7629874372b13bf6ab286d5
dnhyxc <2640559435@qq.com>
2026-07-25 11:39:04 +0800

    feat: 更改输入框选中文本 selection 样式

--- apps/frontend/src/components/ui/input.tsx
+++ apps/frontend/src/components/ui/input.tsx
@@ -31,7 +31,7 @@ function Input({
 			value={value}
 			maxLength={maxLength}
 			className={cn(
-				'caret-textcolor file:text-textcolor placeholder:text-textcolor/60 selection:bg-theme selection:text-default dark:bg-input/30 border border-theme h-9 w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+				'caret-textcolor file:text-textcolor placeholder:text-textcolor/60 dark:bg-input/30 border border-theme h-9 w-full min-w-0 rounded-md bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
 				'focus-visible:border-theme/50 focus-visible:ring-theme/30 focus-visible:ring-[3px]',
 				'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
 				shouldShowCount && 'pr-14',
